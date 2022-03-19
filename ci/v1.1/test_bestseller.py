3f67cf221e780bf9a20d7adce1a9b12107de0468
ara118 <ara118@sfu.ca>
2022-03-18 18:44:31 -0700

    Adding supporting changes for testing the bestseller microservice

---
+++ ci/v1.1/test_bestseller.py
@@ -0,0 +1,51 @@
+"""
+Test the *_bestseller routines.
+
+These tests are invoked by running `pytest` with the
+appropriate options and environment variables, as
+defined in `conftest.py`.
+"""
+
+# Standard libraries
+
+# Installed packages
+import pytest
+
+# Local modules
+import bestseller
+
+
+@pytest.fixture
+def mserv(request, bestseller_url, auth):
+    return bestseller.BestSeller(bestseller_url, auth)
+
+
+@pytest.fixture
+def bestselling(request):
+    return ('The Alchemist', '1000', '5')
+
+
+def test_best_run(mserv, bestselling):
+    trc, b_id = mserv.create_bestseller(bestselling[0], bestselling[1],
+                                        bestselling[2])
+    assert trc == 200
+    trc, title, copies, rating = mserv.read_bestseller(b_id)
+    assert (trc == 200 and title == bestselling[0] and copies == bestselling[1]
+            and rating == bestselling[2])
+    mserv.delete(b_id)
+    # No status to check
+
+
+@pytest.fixture
+def bestseller_a(request):
+    # Recorded 1967
+    return ('Suspense', '2000')
+
+
+@pytest.fixture
+def m_id_oa(request, mserv, bestseller_a):
+    trc, b_id = mserv.create(bestseller_a[0], bestseller_a[1])
+    assert trc == 200
+    yield b_id
+    # Cleanup called after the test completes
+    mserv.delete(b_id)
---
+++ s3/app.py
@@ -0,0 +1,191 @@
+"""
+SFU CMPT 756
+Sample application---user service.
+"""
+
+# Standard library modules
+import logging
+import sys
+import time
+
+# Installed packages
+from flask import Blueprint
+from flask import Flask
+from flask import request
+from flask import Response
+
+import jwt
+
+from prometheus_flask_exporter import PrometheusMetrics
+
+import requests
+
+import simplejson as json
+
+# The application
+
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Reader process')
+
+bp = Blueprint('app', __name__)
+
+db = {
+    "name": "http://cmpt756db:30002/api/v1/datastore",
+    "endpoint": [
+        "read",
+        "write",
+        "delete",
+        "update"
+    ]
+}
+
+
+@bp.route('/', methods=['GET'])
+@metrics.do_not_track()
+def hello_world():
+    return ("If you are reading this in a browser, your service is "
+            "operational. Switch to curl/Postman/etc to interact using the "
+            "other HTTP verbs.")
+
+
+@bp.route('/health')
+@metrics.do_not_track()
+def health():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/readiness')
+@metrics.do_not_track()
+def readiness():
+    return Response("", status=200, mimetype="application/json")
+
+
+@bp.route('/<reader_id>', methods=['PUT'])
+def update_reader(reader_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}), status=401,
+                        mimetype='application/json')
+    try:
+        content = request.get_json()
+        email = content['email']
+        fname = content['fname']
+        lname = content['lname']
+        libaccountno = content['libaccountno']
+        membershipexp = content['membershipexp']
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    url = db['name'] + '/' + db['endpoint'][3]
+    response = requests.put(
+        url,
+        params={"objtype": "reader", "objkey": reader_id},
+        json={"email": email,
+              "fname": fname,
+              "lname": lname,
+              "libaccountno": libaccountno,
+              "membershipexp": membershipexp})
+    return (response.json())
+
+
+@bp.route('/', methods=['POST'])
+def create_reader():
+    """
+    Create a reader.
+    If a record already exists with the same fname, lname, and email,
+    the old UUID is replaced with a new one.
+    """
+    try:
+        content = request.get_json()
+        lname = content['lname']
+        email = content['email']
+        fname = content['fname']
+        libaccountno = content['libaccountno']
+        membershipexp = content['membershipexp']
+    except Exception:
+        return json.dumps({"message": "error reading arguments"})
+    url = db['name'] + '/' + db['endpoint'][1]
+    response = requests.post(
+        url,
+        json={"objtype": "reader",
+              "lname": lname,
+              "email": email,
+              "fname": fname,
+              "libaccountno": libaccountno,
+              "membershipexp": membershipexp})
+    return (response.json())
+
+
+@bp.route('/<reader_id>', methods=['DELETE'])
+def delete_reader(reader_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(json.dumps({"error": "missing auth"}),
+                        status=401,
+                        mimetype='application/json')
+    url = db['name'] + '/' + db['endpoint'][2]
+
+    response = requests.delete(url,
+                               params={"objtype": "reader",
+                                       "objkey": reader_id})
+    return (response.json())
+
+
+@bp.route('/<reader_id>', methods=['GET'])
+def get_reader(reader_id):
+    headers = request.headers
+    # check header here
+    if 'Authorization' not in headers:
+        return Response(
+            json.dumps({"error": "missing auth"}),
+            status=401,
+            mimetype='application/json')
+    payload = {"objtype": "reader", "objkey": reader_id}
+    url = db['name'] + '/' + db['endpoint'][0]
+    response = requests.get(url, params=payload)
+    return (response.json())
+
+
+@bp.route('/login', methods=['PUT'])
+def login():
+    try:
+        content = request.get_json()
+        uid = content['uid']
+    except Exception:
+        return json.dumps({"message": "error reading parameters"})
+    url = db['name'] + '/' + db['endpoint'][0]
+    response = requests.get(url, params={"objtype": "reader", "objkey": uid})
+    data = response.json()
+    if len(data['Items']) > 0:
+        encoded = jwt.encode({'reader_id': uid, 'time': time.time()},
+                             'secret',
+                             algorithm='HS256')
+    return encoded
+
+
+@bp.route('/logoff', methods=['PUT'])
+def logoff():
+    try:
+        content = request.get_json()
+        _ = content['jwt']
+    except Exception:
+        return json.dumps({"message": "error reading parameters"})
+    return {}
+
+
+# All database calls will have this prefix.  Prometheus metric
+# calls will not---they will have route '/metrics'.  This is
+# the conventional organization.
+app.register_blueprint(bp, url_prefix='/api/v1/reader/')
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        logging.error("Usage: app.py <service-port>")
+        sys.exit(-1)
+
+    p = int(sys.argv[1])
+    # Do not set debug=True---that will disable the Prometheus metrics
+    app.run(host='0.0.0.0', port=p, threaded=True)
